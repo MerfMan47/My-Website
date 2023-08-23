5a30fa1bb04cd3dbcdab79ddeeee5b75c7585c28
JR McGregor <43829830+MerfMan47@users.noreply.github.com>
2023-08-22 18:39:28 -0700

    Add files via upload

--- My Website TEST BRANCH/email.php
+++ My Website TEST BRANCH/email.php
@@ -1,9 +1,89 @@
 <?php
+// if (isset($_POST['email'])) {
+
+//     // EDIT THE FOLLOWING TWO LINES:
+//     $email_to = "inquiry@joshuamcgregor.com";
+//     $email_subject = "Website Message";
+
+//     function problem($error)
+//     {
+//         echo "We're sorry, but there were error(s) found with the form you submitted. ";
+//         echo "These errors appear below.<br><br>";
+//         echo $error . "<br><br>";
+//         echo "Please go back and fix these errors.<br><br>";
+//         die();
+//     }
+
+//     // validation expected data exists
+//     if (
+//         !isset($_POST['name']) ||
+//         !isset($_POST['email']) ||
+//         !isset($_POST['message'])
+//     ) {
+//         problem(`We're sorry, but there appears to be a problem with the form you submitted.`);
+//     }
+
+//     $name = $_POST['name']; // required
+//     $email = $_POST['email']; // required
+//     $message = $_POST['message']; // required
+
+//     $error_message = "";
+//     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+//     if (!preg_match($email_exp, $email)) {
+//         $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+//     }
+
+//     $string_exp = "/^[A-Za-z .'-]+$/";
+
+//     if (!preg_match($string_exp, $name)) {
+//         $error_message .= 'The Name you entered does not appear to be valid.<br>';
+//     }
+
+//     if (strlen($message) < 2) {
+//         $error_message .= 'The Message you entered do not appear to be valid.<br>';
+//     }
+
+//     if (strlen($error_message) > 0) {
+//         problem($error_message);
+//     }
+
+//     $email_message = "Form details below.\n\n";
+
+//     function clean_string($string)
+//     {
+//         $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+//         return str_replace($bad, "", $string);
+//     }
+
+//     $email_message .= "Name: " . clean_string($name) . "\n";
+//     $email_message .= "Email: " . clean_string($email) . "\n";
+//     $email_message .= "Message: " . clean_string($message) . "\n";
+
+//     // create email headers
+//     $headers = 'From: ' . $email . "\r\n" .
+//         'Reply-To: ' . $email . "\r\n" .
+//         'X-Mailer: PHP/' . phpversion();
+//     @mail($email_to, $email_subject, $email_message, $headers);
+
+
+//TESTING ZONE
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP.php;
+use PHPMailer\PHPMailer\Exception.php;
+
+require_once './PHPMailer/PHPMailer.php';
+require_once './PHPMailer/SMTP.php';
+require_once './PHPMailer/Exception.php';
+
+
+
 if (isset($_POST['email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "fake email";
-    $email_subject = "Website Message";
+    // $email_to = "fakeEmail";
+    // $email_subject = "Website Message";
 
     function problem($error)
     {
@@ -60,14 +140,39 @@ if (isset($_POST['email'])) {
     $email_message .= "Email: " . clean_string($email) . "\n";
     $email_message .= "Message: " . clean_string($message) . "\n";
 
-    // create email headers
-    $headers = 'From: ' . $email . "\r\n" .
-        'Reply-To: ' . $email . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $email_subject, $email_message, $headers);
-?>
+    // // create email headers
+    // $headers = 'From: ' . $email . "\r\n" .
+    //     'Reply-To: ' . $email . "\r\n" .
+    //     'X-Mailer: PHP/' . phpversion();
+    // @mail($email_to, $email_subject, $email_message, $headers);
+    
+    $mail = new PHPMailer();
 
-    <!-- INCLUDE YOUR SUCCESS MESSAGE BELOW -->
+    try {
+        $mail->SMTPDebug = 2;
+        $mail->isSMTP();
+        $mail->Host = "smtp.office365.com";
+        $mail->SMTPAuth = true;
+        $mail->Username = 'fakeEmail';
+        $mail->Password = 'fakePassword';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        $mail->setFrom($email, $name);
+        $mail->addAddress('fakeEmail');
+        $mail->isHTML(true);
+        $mail->Subject = $email_subject;
+        $mail->Body = $email_message;
+
+        $mail->send();
+        echo "Thanks for getting in touch. I'll get back to you soon.";
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error. {$mail->ErrorInfor}";
+    }
+
+?>
+<!-- 
+    INCLUDE YOUR SUCCESS MESSAGE BELOW -->
 
     Thanks for getting in touch. I'll get back to you soon.
 
